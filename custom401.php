f41b153eb42c1b3a2f9d2aa4e187263c4408376b
Hugh Owens <8038754+HughO95@users.noreply.github.com>
2024-06-10 16:42:47 +0100

    webpages
    webpages of my portfolio

---
+++ custom401.php
@@ -0,0 +1,6 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<h1>Error 401 - Unauthorized</h1>
+<p>You are unauthorized to view this page. We will take you back to the homepage</p>
+	<p>If this pages does not refresh in a few seconds click the link below</p>
+	<a href="index.html">Back to home page</a><br>
+	<?php header( 'Refresh:5; url=index.html' );?>
