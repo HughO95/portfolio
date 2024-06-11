b390bd7a1da7845a6fc9f7e389084ecf42c0342d
HughO95 <hro.webdev@gmail.com>
2024-06-11 16:39:17 +0100

    adding new media queries

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 //javascript document
 
-function showMessage() //start of function for heading message
-{
-theDate= new Date();
-day=theDate.getDate();
-hours=theDate.getHours();
-theMessage=document.getElementById("welcomeMessage")
-if(hours<12)
-{
-	theMessage.innerHTML="Good Morning, Welcome to hugh.cymru!"
-}  // if the time is before 12PM (00:00-11:59) this message is shown
-if((hours>=12)&&(hours<17))
-{
-	theMessage.innerHTML="Good Afternoon, Welcome to hugh.cymru!"
-} // if the time is between 12:00-16:59, this message is shown
-if((hours>=17)&&(hours <=23)) 
-{
-	theMessage.innerHTML="Good Evening, Welcome to hugh.cymru!"
-} // if the time is between 17:00-23:59 this message is shown
-} //end of function for heading message
-
+function showMessage() {
+    var theDate = new Date();
+    var hours = theDate.getHours();
+    var theMessage = document.getElementById("welcomeMessage");
+    if (hours < 12) {
+        theMessage.innerHTML = "Good Morning, Welcome to hugh.cymru!";
+    } else if (hours >= 12 && hours < 17) {
+        theMessage.innerHTML = "Good Afternoon, Welcome to hugh.cymru!";
+    } else {
+        theMessage.innerHTML = "Good Evening, Welcome to hugh.cymru!";
+    }
+}
 
 //hamburger jquery
 $(document).ready(function () {
     $('.hamburger').on('click', function (e) {
-        // this prevents the link from jumping to the top of the page
         e.preventDefault();
-        // If the menu is showing, slide it up. Otherwise, slide it down.
         $('.menu').toggleClass('slide-down');
     });
-}); 
-// end of hamburger jquery
+});
+
+function showMoreSkills() {
+    var hiddenParagraphs = document.querySelectorAll('.card .text p.hidden');
+    var isHidden = hiddenParagraphs[0].style.display === 'none' || hiddenParagraphs[0].classList.contains('hidden');
+    for (var i = 0; i < hiddenParagraphs.length; i++) {
+        hiddenParagraphs[i].style.display = isHidden ? 'block' : 'none';
+    }
+    var viewMoreBtn = document.getElementById('viewMoreBtn');
+    viewMoreBtn.innerText = isHidden ? 'Show less' : 'Click to view more';
+}
